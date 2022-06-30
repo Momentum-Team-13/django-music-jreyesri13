37b1107837591b72dd812c5fced0235c9eafb9e0
Jose Reyes <jreyesri@cbu.edu>
2022-06-30 11:36:09 -0400

    Added the django extensions

--- django_music/settings.py
+++ django_music/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Extensions
+    'django_extensions',
+
+    # Project-specific
     'albums',
 ]
 
