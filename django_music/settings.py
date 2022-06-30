155b55d5a7d7eb5425639335e65f7456b072f63a
Jose Reyes <jreyesri@cbu.edu>
2022-06-30 15:09:42 -0400

    Added the debug toolbar extension

--- django_music/settings.py
+++ django_music/settings.py
@@ -40,12 +40,14 @@ INSTALLED_APPS = [
 
     # Extensions
     'django_extensions',
+    'debug_toolbar',
 
     # Project-specific
     'albums',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -122,6 +124,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+# Debug toolbar config
+
+INTERNAL_IPS = [
+    # ...
+    '127.0.0.1',
+    # ...
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
